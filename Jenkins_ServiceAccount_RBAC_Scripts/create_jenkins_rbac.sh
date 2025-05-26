404b94193a97ed6191fb5be16a8b439b399d4366
GODFREY IFEANYI <90422561+Godfrey22152@users.noreply.github.com>
2025-05-26 20:21:44 +0100

    Update create_jenkins_rbac.sh

--- Jenkins_ServiceAccount_RBAC_Scripts/create_jenkins_rbac.sh
+++ Jenkins_ServiceAccount_RBAC_Scripts/create_jenkins_rbac.sh
@@ -2,12 +2,12 @@
 
 # Define namespace
 NAMESPACE="webapps"
-ROLE="app-role"
-ROLEBINDING="app-rolebinding"
+ROLE="jenkins-role"
+ROLEBINDING="jenkins-rolebinding"
 SECRET="jenkins-secret"
 SERVICEACCOUNT="jenkins"
 RBAC_CONFIG_FILE="jenkins_rbac_config.yaml"
-
+PROCESS_NAME="Jenkins ServiceAccount"
 
 # Check if namespace exists, if not, create it
 kubectl get namespace $NAMESPACE >/dev/null 2>&1
@@ -18,8 +18,8 @@ else
   echo "Namespace $NAMESPACE already exists."
 fi
 
-# Create the resources using the github-actions_rbac_config.yaml file
-echo "Creating Github Actions ServiceAccount, Role, RoleBinding, and Secret..."
+# Create the resources using the Jenkins_rbac_config.yaml file
+echo "Creating $PROCESS_NAME, Role, RoleBinding, and Secret..."
 kubectl apply -f $RBAC_CONFIG_FILE
 
 # Verify the creation of the ServiceAccount, Role, and RoleBinding
@@ -35,9 +35,9 @@ sleep 5
 SECRET_NAME=$(kubectl get secret -n $NAMESPACE | grep $SECRET | awk '{print $1}')
 
 if [ -z "$SECRET_NAME" ]; then
-  echo "Error: Could not find the $SECRET secret for the ServiceAccount $SERVICEACCOUNT."
+  echo "Error: Could not find the $SECRET secret for the $PROCESS_NAME."
 else
-  echo "ServiceAccount secret created: $SECRET_NAME"
+  echo "$PROCESS_NAME secret created: $SECRET_NAME"
 
   # Retrieve the secret token
   SECRET_TOKEN=$(kubectl get secret $SECRET_NAME -n $NAMESPACE -o jsonpath='{.data.token}' | base64 --decode)
@@ -45,9 +45,9 @@ else
   if [ -z "$SECRET_TOKEN" ]; then
     echo "Error: Could not retrieve the token from the secret."
   else
-    echo "Secret token for the ServiceAccount $SERVICEACCOUNT:"
+    echo "Secret token for the $PROCESS_NAME:"
     echo "$SECRET_TOKEN"
   fi
 fi
 
-echo "$SERVICEACCOUNT service account and RBAC resources created successfully!"
+echo "$PROCESS_NAME and RBAC resources created successfully!"
